b3800d717cbbeb4b61854030d8c23d35042c6225
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-24 17:39:01 -0400

    user delete request complete

--- data/UserRouter.js
+++ data/UserRouter.js
@@ -53,4 +53,21 @@ router.post("/", (req, res) => {
   }
 });
 
+router.delete("/:id", (req, res) => {
+  const userId = req.params.id;
+  db.remove(userId)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist."
+        });
+      } else {
+        res.end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The user could not be removed" });
+    });
+});
+
 module.exports = router;
